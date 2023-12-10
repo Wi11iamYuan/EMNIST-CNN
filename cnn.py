01d6a1ffe1cf7158544d1b0109cf9345544dfd5c
William Yuan <66759156+Wi11iamYuan@users.noreply.github.com>
2023-12-09 16:39:15 -0800

    progress

--- cnn.py
+++ cnn.py
@@ -1,14 +1,26 @@
 #%%
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers 
+
 from emnist import list_datasets
 from emnist import extract_training_samples
 from emnist import extract_test_samples
 
-import matplotlib.pyplot as plt
-
 #%%
 
-images, labels = extract_training_samples('letters')
+images_train, labels_train = extract_training_samples('letters')
+images_test, labels_test = extract_test_samples('letters')
 #%%
-plt.imshow(images[0])
-print(labels[0])
+plt.imshow(images_train[0])
+print(labels_train[0])
+
+plt.imshow(images_test[0])
+print(labels_test[0])
+# %%
+images_train, images_test = images_train / 255.0 , images_test / 255.0
+# %%
+
 # %%
